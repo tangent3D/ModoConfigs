11bedefa71a8d0a679ce413cc72c3c81cd23d66c
Nicholas Hull <interfectum@gmail.com>
2021-04-19 01:21:14 -0700

    Added explode before baking

--- Scripts/bake_tangent_space_normals.py
+++ Scripts/bake_tangent_space_normals.py
@@ -5,10 +5,16 @@ from definitions import *
 # Set bake sources and targets and corresponding materials 
 lx.eval('@set_sources_targets.py')
 
+# Explode to frame 1
+lx.eval('select.time 0.041667 0 0')
+
 lx.eval('shader.setVisible '+getTEX_Curvature().id+' false')
 getBAKE_TEX_Normal().select(replace=True)
 lx.eval('bakeItem.bake true')
 lx.eval('shader.setVisible '+getTEX_Curvature().id+' true')
 lx.eval('shader.setVisible '+getIMG_Normal().id+' true')
+
+# Un-explode to frame 0
+lx.eval('select.time 0 0 0')
 
 lx.eval('@show_LO.py')
