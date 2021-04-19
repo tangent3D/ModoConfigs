6e8e87be6f64d23b212ac8160ad5ef7a73957a71
Nicholas Hull <interfectum@gmail.com>
2021-04-19 02:19:14 -0700

    Hide wireframe texture when baking curvature

--- Scripts/bake_curvature.py
+++ Scripts/bake_curvature.py
@@ -11,13 +11,17 @@ lx.eval('@bake_settings_final.py')
 # Explode to frame 1
 lx.eval('select.time 0.041667 0 0')
 
+lx.eval('shader.setVisible '+getTEX_Wireframe().id+' false')
+
 lx.eval('shader.setVisible '+getTEX_Curvature().id+' true')
 
 lx.eval('select.drop item')
 
 lx.eval('select.subItem '+getBAKE_RO_Curvature().id+' set')
 
-lx.eval('bakeItem.bake true')
+lx.eval('bakeItem.bake true'
+
+lx.eval('shader.setVisible '+getTEX_Wireframe().id+' true')
 
 # Un-explode to frame 0
 lx.eval('select.time 0 0 0')
--- Scripts/bake_curvature_draft.py
+++ Scripts/bake_curvature_draft.py
@@ -11,6 +11,8 @@ lx.eval('@bake_settings_draft.py')
 # Explode to frame 1
 lx.eval('select.time 0.041667 0 0')
 
+lx.eval('shader.setVisible '+getTEX_Wireframe().id+' false')
+
 lx.eval('shader.setVisible '+getTEX_Curvature().id+' true')
 
 lx.eval('select.drop item')
@@ -18,6 +20,8 @@ lx.eval('select.drop item')
 lx.eval('select.subItem '+getBAKE_RO_Curvature().id+' set')
 
 lx.eval('bakeItem.bake true')
+
+lx.eval('shader.setVisible '+getTEX_Wireframe().id+' true')
 
 # Un-explode to frame 0
 lx.eval('select.time 0 0 0')
