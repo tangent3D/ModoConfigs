e6ff5d4e03e30209355c7136ea1f03cabd2d06fe
Nicholas Hull <interfectum@gmail.com>
2021-05-24 13:58:20 -0700

    Create clean_uv_maps.py
    Script to remove UV maps from HI and Decal meshes

---
+++ Scripts/clean_uv_maps.py
@@ -0,0 +1,16 @@
+# python
+
+from definitions import *
+
+lx.eval('select.drop item')
+
+# Remove UV map 'Texture' from all HI and Decal meshes
+getMESH_HI().select(replace=True)
+getMESH_Decals().select()
+lx.eval('select.itemHierarchy')
+
+try:
+	lx.eval('select.vertexMap Texture txuv add')
+	lx.eval('vertMap.delete txuv')
+except:
+	pass
