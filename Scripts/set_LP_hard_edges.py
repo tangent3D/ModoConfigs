a2d5f55fda84c93820287c8d471115681dbb486d
Nicholas Hull <interfectum@gmail.com>
2021-06-15 16:38:30 -0700

    Update hard edge scripts
    Remove existing hard edges & vertex normals before setting hard edges

--- Scripts/set_LP_hard_edges.py
+++ Scripts/set_LP_hard_edges.py
@@ -9,6 +9,23 @@ lx.eval('material.smoothCrease hardedges:true')
 # Disable area weighting
 lx.eval('material.smoothWeight area false')
 
+# Before we set the hard edges, let's make sure to remove any existing Normal or Hard Edge vertex maps
+getMESH_LO().select(replace=True)
+lx.eval('select.itemHierarchy')
+
+try:
+	lx.eval('select.vertexMap "Hard Edge" hard replace')
+	lx.eval('vertMap.delete hard')
+except:
+	pass
+
+try:
+	lx.eval('select.vertexMap "Vertex Normal" norm replace')
+	lx.eval('vertMap.delete norm')
+except:
+	pass
+
+# Define the new hard edges
 getMESH_LO().select(replace=True)
 lx.eval('select.itemHierarchy')
 lx.eval('select.vertexMap Texture txuv replace')
@@ -16,5 +33,11 @@ lx.eval('hardedge.setDefault uvisland')
 lx.eval('hardedge.setDefault soft:uvisland')
 lx.eval('hardedge.set soft')
 lx.eval('hardedge.set hard')
+
+# # Set the Vertex Normals
+# # Removed this in case I need to manually soften edges
+# getMESH_LO().select(replace=True)
+# lx.eval('select.itemHierarchy')
+# lx.eval('vertMap.normals "Vertex Normal" true 1.0 Texture false')
 
 lx.eval('select.drop item')
--- Scripts/set_LP_hard_edges_weighted.py
+++ Scripts/set_LP_hard_edges_weighted.py
@@ -9,6 +9,23 @@ lx.eval('material.smoothCrease hardedges:true')
 # Set area weighting
 lx.eval('material.smoothWeight area true')
 
+# Before we set the hard edges, let's make sure to remove any existing Normal or Hard Edge vertex maps
+getMESH_LO().select(replace=True)
+lx.eval('select.itemHierarchy')
+
+try:
+	lx.eval('select.vertexMap "Hard Edge" hard replace')
+	lx.eval('vertMap.delete hard')
+except:
+	pass
+
+try:
+	lx.eval('select.vertexMap "Vertex Normal" norm replace')
+	lx.eval('vertMap.delete norm')
+except:
+	pass
+
+# Define the new hard edges
 getMESH_LO().select(replace=True)
 lx.eval('select.itemHierarchy')
 lx.eval('select.vertexMap Texture txuv replace')
@@ -16,5 +33,10 @@ lx.eval('hardedge.setDefault uvisland')
 lx.eval('hardedge.setDefault soft:uvisland')
 lx.eval('hardedge.set soft')
 lx.eval('hardedge.set hard')
+
+# # Set the Vertex Normals
+# getMESH_LO().select(replace=True)
+# lx.eval('select.itemHierarchy')
+# lx.eval('vertMap.normals "Vertex Normal" true 1.0 Texture false')
 
 lx.eval('select.drop item')
