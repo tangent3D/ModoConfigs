8fb6694fb00a0e43f54ef68d2761e5a0df99cf13
tangent3D <interfectum@gmail.com>
2023-02-27 12:44:40 -0800

    Freeze normals with external script

--- Scripts/export_merged.py
+++ Scripts/export_merged.py
@@ -4,11 +4,13 @@ from definitions import *
 
 scene = modo.Scene()
 
+# Freeze LO vertex normals before export
+lx.eval(@set_vertex_normals)
+
 getMESH_LO().select(replace=True)
 lx.eval('select.itemHierarchy')
 # Deselect the locator so it's not merged
 modo.item.Item('locator.LO').deselect()
-lx.eval('vertMap.normals "Vertex Normal" true 1.0 {} false')
 lx.eval('item.duplicate false locator false true')
 lx.eval('layer.mergeMeshes true')
 mergedMesh = modo.item.Item(item=None)
@@ -16,7 +18,8 @@ try:
 	lx.eval('export.selected 15 false false false')
 except:
 	pass
-# Select original scene
+
+# Remove merged mesh
 modo.scene.Scene(scene=scene.scene)
 mergedMesh.select(replace=True)
 lx.eval('delete')
