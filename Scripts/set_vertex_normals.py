1c3562321d2b226903d825f9fa2d6078301a6369
tangent3D <interfectum@gmail.com>
2023-03-08 02:09:10 -0800

    Update set_vertex_normals.py

--- Scripts/set_vertex_normals.py
+++ Scripts/set_vertex_normals.py
@@ -7,9 +7,4 @@ getMESH_LO().select(replace=True)
 lx.eval('select.itemHierarchy')
 lx.eval('vertMap.normals "Vertex Normal" true 1.0 {} true')
 
-# # Disabled the following because it causes problems with xNormal os2ts?
-# # Create Mikk Tangent Basis. Note: Tangent Bases must be added before baking!
-# lx.eval('select.vertexMap Texture txuv add')
-# lx.eval('mesh.mikktspacegen')
-
 lx.eval('select.drop item')
